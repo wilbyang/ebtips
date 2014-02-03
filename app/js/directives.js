c9456213280a7df7fe1c48de589c3884f3364a0c
lonn <lonnglau@gmail.com>
2014-02-03 18:23:23 +0800

    use directives

--- app/js/directives.js
+++ app/js/directives.js
@@ -8,4 +8,13 @@ angular.module('myApp.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  }])
+  .directive('myContent', function() {
+  	return {
+  		restrict: 'E',
+  		scope: {
+  			contentInfo: '=info'
+  		},
+  		templateUrl: 'partials/content.html'
+  	};
+  });
